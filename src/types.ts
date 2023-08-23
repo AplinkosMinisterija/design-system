452e45fed344db043df4eaae1c87223d2fd3b41b
Dovile <dovile.maminskaite@am.lt>
2023-08-23 21:08:14 +0300

    removed unnecessary colors from theme

--- src/types.ts
+++ src/types.ts
@@ -6,7 +6,6 @@ export interface Theme {
     transparent: string;
     danger: string;
     success: string;
-    accent: string;
     hover: {
       primary: string;
       secondary: string;
@@ -21,16 +20,10 @@ export interface Theme {
       secondary: string;// for paragraphs and other regular texts
       tertiary: string; // for less important explanatory texts
       labels: string; // for input label
-      accent: string; // for any kind of highlighted text
       error: string; // for error
-      input: string; // for text inside input
-      placeholder: string; // for input placeholder
-      link: string; // for link text
       [key: string] : string;
     };
     border: string; // for input and other bordered elements
-    input: string; // input background
-    shadow: string;
     [key: string] : string | {[key: string] : string};
   };
 }
