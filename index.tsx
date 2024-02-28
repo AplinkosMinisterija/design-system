db6f29ce1334aaaff9cc0da477efdd6e220d6d21
Dovile <dovile.maminskaite@am.lt>
2024-02-28 18:37:17 +0100

    added checkbox and switch components

--- index.tsx
+++ index.tsx
@@ -1,5 +1,7 @@
 import Button from "./components/Button";
+import Checkbox from "./components/Checkbox";
+import Switch from "./components/Switch";
 
 export * from './types';
-export default { Button };
+export default { Button, Checkbox, Switch };
 
