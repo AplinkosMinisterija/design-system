f20818dea92101527681776b7f0ee5f839d79efd
Dovile <dovile.maminskaite@am.lt>
2024-10-10 21:04:21 +0300

    ButtonMultiSelect

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -36,6 +36,7 @@ export const theme: Theme = {
         text: 'white',
         border: 'transparent',
         hover: '#a9d8fd',
+        checked: '#008bff',
       },
       [ButtonVariants.SECONDARY]: {
         background: 'white',
@@ -43,6 +44,7 @@ export const theme: Theme = {
         border: 'transparent',
         hover: '#53B1FD',
         hoverText: 'white',
+        checked: '#a9d8fd',
       },
       [ButtonVariants.OUTLINE]: {
         background: 'transparent',
@@ -51,6 +53,9 @@ export const theme: Theme = {
         hover: 'transparent',
         hoverText: '#525469',
         hoverBorder: '#525469',
+        checked: '#a9d8fd',
+        checkedBorder: '#53B1FD',
+        checkedText: '#525469',
       },
       [ButtonVariants.TRANSPARENT]: {
         background: 'transparent',
@@ -58,6 +63,7 @@ export const theme: Theme = {
         border: 'transparent',
         hover: 'transparent',
         hoverText: '#53B1FD',
+        checkedText: '#008bff',
       },
       [ButtonVariants.COLUMNS]: {
         background: 'white',
@@ -70,6 +76,8 @@ export const theme: Theme = {
           text: 'white',
           background: '#53B1FD',
         },
+        checkedText: '#53B1FD',
+        checkedBorder: '#53B1FD',
       },
     },
     fields: {
@@ -142,6 +150,9 @@ export const theme: Theme = {
     fields: 400,
     fieldLabels: 400,
     buttons: 400,
+  },
+  gap: {
+    buttonMultiSelect: 1.2,
   },
 };
 
--- stories/Fields.Checkbox.stories.tsx
+++ stories/Fields.Checkbox.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import Checkbox from '../src/components/Checkbox';
 import StoryWrapper from '../src/components/common/StoryWrapper';
+import { useState } from 'react';
+import { ButtonVariants } from '../.storybook/preview';
 
 const meta: Meta<typeof Checkbox> = {
   component: Checkbox,
@@ -11,19 +13,29 @@ const meta: Meta<typeof Checkbox> = {
 export default meta;
 type Story = StoryObj<typeof Checkbox>;
 
+const StoryComponent = () => {
+  const [selected, setSelected] = useState<boolean>();
+  return (
+    <StoryWrapper>
+      <Checkbox
+        label={'label'}
+        value={selected}
+        name={'checkbox'}
+        // description={
+        //   'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+        // }
+        displayAsButton={true}
+        width={'100px'}
+        variant={ButtonVariants.OUTLINE}
+        onChange={(val) => {
+          setSelected(val);
+        }}
+      />
+    </StoryWrapper>
+  );
+};
+
 export const CheckboxStory: Story = {
   name: 'Checkbox',
-  render: () => {
-    return (
-      <StoryWrapper>
-        <Checkbox
-          label={'label'}
-          description={
-            'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
-          }
-          onChange={() => {}}
-        />
-      </StoryWrapper>
-    );
-  },
+  render: StoryComponent,
 };
