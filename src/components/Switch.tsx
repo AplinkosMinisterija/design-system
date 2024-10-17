bc943463ecbfe86711570cf8d94d5d485bbbb078
Jaroslav Siroic <jaroslav.siroic@gmail.com>
2024-10-17 14:17:04 +0300

    updated switch

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,43 +1,92 @@
-import styled from 'styled-components';
-import { ChangeEventHandler } from 'react';
+import styled, { useTheme } from 'styled-components';
 
 export interface SwitchProps {
   value?: boolean;
   name?: string;
-  onChange: ChangeEventHandler<HTMLInputElement>;
+  onChange: (value: boolean) => void;
   disabled?: boolean;
-  label?: any;
-  error?: boolean;
+  label?: string;
   className?: string;
-  intermediate?: boolean;
+  labelStyle?: any;
+  labelPosition: 'left' | 'right';
+  trackColorActive?: string;
+  trackColorInactive?: string;
+  thumbColor?: string;
 }
 
-const Switch = ({ value, name, onChange }: SwitchProps) => {
+const Switch = ({
+  value,
+  name,
+  onChange,
+  disabled,
+  label,
+  className,
+  labelStyle,
+  labelPosition = 'right',
+  trackColorActive,
+  trackColorInactive,
+  thumbColor,
+}: SwitchProps) => {
+  const onSwitchClick = () => {
+    if (!disabled) onChange(!value);
+  };
+
+  const renderLabel = () => (
+    <Label style={labelStyle} onClick={onSwitchClick} $disabled={disabled}>
+      {label}
+    </Label>
+  );
+
   return (
-    <>
-      <SwitchContainer>
-        <InputCheckbox type="checkbox" name={name} checked={value} onChange={onChange} />
-        <Slider />
+    <SwitchWrapper className={className} $disabled={disabled}>
+      {label && labelPosition === 'left' && renderLabel()}
+      <SwitchContainer $disabled={disabled}>
+        <InputCheckbox
+          $color={trackColorActive}
+          type="checkbox"
+          name={name}
+          disabled={disabled}
+          checked={value}
+          onChange={onSwitchClick}
+        />
+        <Slider $color={trackColorInactive} $thumbColor={thumbColor} />
       </SwitchContainer>
-    </>
+      {label && labelPosition === 'right' && renderLabel()}
+    </SwitchWrapper>
   );
 };
 
-const SwitchContainer = styled.label`
+const SwitchWrapper = styled.div<{ $disabled: boolean }>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  opacity: ${({ $disabled }) => ($disabled ? 0.48 : 1)};
+`;
+
+const SwitchContainer = styled.label<{ $disabled: boolean }>`
+  cursor: ${({ $disabled }) => ($disabled ? 'not-allowed' : 'pointer')};
   position: relative;
   display: inline-block;
   width: 44px;
   height: 24px;
 `;
 
-const Slider = styled.span`
+const Label = styled.label<{ $disabled: boolean }>`
+  user-select: none;
+  cursor: ${({ $disabled }) => ($disabled ? 'not-allowed' : 'pointer')};
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: 1.6rem;
+  font-weight: 500;
+`;
+
+const Slider = styled.span<{ $color?: string; $thumbColor?: string }>`
   position: absolute;
-  cursor: pointer;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-  background-color: #ccc;
+  background-color: ${({ $color }) => $color ?? '#ccc'};
   transition: 0.4s;
   border-radius: 24px;
 
@@ -48,23 +97,23 @@ const Slider = styled.span`
     width: 18px;
     left: 3px;
     bottom: 3px;
-    background-color: white;
+    background-color: ${({ $thumbColor }) => $thumbColor ?? 'white'};
     transition: 0.4s;
     border-radius: 50%;
   }
 `;
 
-const InputCheckbox = styled.input`
+const InputCheckbox = styled.input<{ $disabled: boolean; $color?: string }>`
   opacity: 0;
   width: 0;
   height: 0;
 
   &:checked + ${Slider} {
-    background-color: ${({ theme }) => theme.colors.primary};
+    background-color: ${({ theme, $color }) => $color ?? theme.colors.primary};
   }
 
   &:focus + ${Slider} {
-    box-shadow: 0 0 1px ${({ theme }) => theme.colors.primary};
+    box-shadow: 0 0 1px ${({ theme, $color }) => $color ?? theme.colors.primary};
   }
 
   &:checked + ${Slider}:before {
