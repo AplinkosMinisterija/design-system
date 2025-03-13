85d42932a5beedd8a68887ce6370ecf55a17e251
dovilemely <dovilemelynyte@gmail.com>
2025-03-13 15:42:32 +0200

    table loader added

---
+++ src/components/tables/components/TableLoader.tsx
@@ -0,0 +1,25 @@
+import LoaderComponent from 'src/components/common/LoaderComponent';
+import styled from 'styled-components';
+
+const TableLoader = () => {
+  return (
+    <LoaderWrapper>
+      <LoaderComponent />
+    </LoaderWrapper>
+  );
+};
+
+const LoaderWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9;
+  opacity: 0.7;
+`;
+
+export default TableLoader;
